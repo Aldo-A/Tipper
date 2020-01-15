3e5e363744fc146001ccc80016041187b655f196
Aldo <aalmeidarojo18@students.desu.edu>
2020-01-15 18:37:14 -0500

    first commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Aldo Almeida on 1/15/20.
+//  Copyright © 2020 Aldo Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
